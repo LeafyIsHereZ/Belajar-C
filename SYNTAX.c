ed6a8b67e678a47e9c3e59c3f28489059e2955c7
Muhammad Quwais Safutra <44535941+LeafyIsHereZ@users.noreply.github.com>
2018-11-03 00:02:00 +0700

    Create SYNTAX.c

---
+++ SYNTAX.c
@@ -0,0 +1,8 @@
+/* setiap bahasa c. C membutuhkan satu syarat! yaitu Sebuah liblary. ada juga sih yang ga ada. tapi lebih baik di pake aja deh */
+
+#include <stdio.h> // ini adalah liblary C
+
+int main(void){
+	printf("hello world!"); // print 'hello world' nanti di output
+	return 0; // sebuah program yang kita buat tidak akan terjadi masalah
+}
